685d66190fe095ffa65d719f8c75a603b57e0f65
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-10-06 07:28:33 +0200

    Responsive Nav

--- common/components/Nav.tsx
+++ common/components/Nav.tsx
@@ -1,6 +1,8 @@
 import React, {FunctionComponent, useCallback, useEffect, useState} from "react";
 import BgMobileLight from '../assets/bg-mobile-light.jpg';
 import BgMobileDark from '../assets/bg-mobile-dark.jpg';
+import BgDesktopDark from '../assets/bg-desktop-dark.jpg';
+import BgDesktopLight from '../assets/bg-desktop-light.jpg';
 import LightMoon from '../assets/icon-moon.svg';
 import SunDark from '../assets/icon-sun.svg';
 import Image from "next/image";
@@ -44,18 +46,24 @@ const Nav: FunctionComponent = () => {
   }, [theme])
 
   return (
-      <header className="relative z-0 w-[100vw]">
-        <div className="absolute z-0 w-full">
-          <div className="dark:hidden">
+      <header className="z-0 w-[100vw] h-full">
+        <nav className="z-0 w-full">
+          <div className="absolute z-10 py-10 px-5 w-full flex justify-between items-center">
+            <h1 className="tracking-[12.5px] text-2xl text-[#FFFFFF] font-semibold">TODO</h1>
+            {renderButton()}
+          </div>
+          <div className="dark:hidden lg:hidden">
             <Image src={BgMobileLight} layout="responsive"/>
           </div>
-          <div className={"hidden dark:block"}>
+          <div className={"hidden dark:block dark:lg:hidden"}>
             <Image src={BgMobileDark} layout="responsive"/>
           </div>
-        </div>
-        <nav className="absolute z-10 py-10 px-5 w-full flex justify-between items-center">
-          <h1 className="tracking-[12.5px] text-2xl text-[#FFFFFF] font-semibold">TODO</h1>
-          {renderButton()}
+          <div className="hidden lg:block dark:hidden">
+            <Image src={BgDesktopLight} layout="responsive"/>
+          </div>
+          <div className={"hidden lg:dark:block"}>
+            <Image src={BgDesktopDark} layout="responsive"/>
+          </div>
         </nav>
       </header>
   )
