4ebecc0effad2e8d781fc366412c1e3f687bd7a9
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-10-06 07:29:37 +0200

    Format code

--- common/hooks/useStore.tsx
+++ common/hooks/useStore.tsx
@@ -3,3 +3,4 @@ import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
