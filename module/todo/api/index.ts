2d980ecd1b1648e2e35cbc6dbf4879ed19010f69
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-10-22 10:33:54 +0200

    Implement env variable

--- module/todo/api/index.ts
+++ module/todo/api/index.ts
@@ -16,7 +16,7 @@ let todoBackendInstance: TodoBackend | undefined = undefined;
 
 export async function getTodoBackend(): Promise<TodoBackend> {
 	if (todoBackendInstance === undefined) {
-		const mod = await import('./backends/' + 'dummy');
+		const mod = await import('./backends/' + process.env.NEXT_PUBLIC_TODO_API_BACKEND_MODULE);
 		todoBackendInstance = new mod.default() as TodoBackend;
 	}
 	return todoBackendInstance;
