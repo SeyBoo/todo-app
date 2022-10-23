106258eb71ba5cc7bf893133bd0335762b775c63
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-10-23 14:13:17 +0200

    active style filter

--- module/todo/components/statusFilter.tsx
+++ module/todo/components/statusFilter.tsx
@@ -1,76 +1,76 @@
-import React, { FunctionComponent, useCallback } from 'react';
+import React, { FunctionComponent, useCallback, useState } from 'react';
 import { useAppDispatch } from '../../../common/hooks/useStore';
 import { loadActiveTodo, loadCompletedTodo, loadTodos } from '../store/thunk';
 
 const StatusFilter: FunctionComponent = () => {
 	const dispatch = useAppDispatch();
+	const buttonDefaultStyle = 'text-[#9495A5] dark:text-[#5B5E7E]';
+	const activeButtonStyle = 'text-[#3A7CFD]';
+	const [activeFilter, setActiveFilter] = useState('all');
 
-	const handleLoadAll = useCallback(async () => {
-		try {
-			await dispatch(loadTodos());
-		} catch (e) {
-			console.error(e);
-		}
-	}, [dispatch]);
-
-	const handleLoadActive = useCallback(async () => {
-		try {
-			await dispatch(loadActiveTodo());
-		} catch (e) {
-			console.error(e);
-		}
-	}, [dispatch]);
-
-	const handleLoadCompleted = useCallback(async () => {
-		try {
-			await dispatch(loadCompletedTodo());
-		} catch (e) {
-			console.error(e);
-		}
-	}, [dispatch]);
-
-	interface filterItem {
-		name: string;
-		active: boolean;
-		action: () => void;
-	}
-
-	const filter: filterItem[] = [
-		{
-			name: 'All',
-			active: true,
-			action: () => handleLoadAll(),
+	const handleLoadAll = useCallback(
+		async (name) => {
+			try {
+				await dispatch(loadTodos());
+				setActiveFilter(name);
+			} catch (e) {
+				console.error(e);
+			}
 		},
-		{
-			name: 'Active',
-			active: false,
-			action: () => handleLoadActive(),
+		[dispatch]
+	);
+
+	const handleLoadActiveTodo = useCallback(
+		async (name) => {
+			try {
+				await dispatch(loadActiveTodo());
+				setActiveFilter(name);
+			} catch (e) {
+				console.error(e);
+			}
 		},
-		{
-			name: 'Completed',
-			active: false,
-			action: () => handleLoadCompleted(),
+		[dispatch]
+	);
+
+	const handleLoadCompletedTodo = useCallback(
+		async (name) => {
+			try {
+				await dispatch(loadCompletedTodo());
+				setActiveFilter(name);
+			} catch (e) {
+				console.error(e);
+			}
 		},
-	];
-
-	const renderButton = (item: filterItem, index: number) => {
-		let defaultStyling = '';
-
-		if (item.active) {
-			defaultStyling = defaultStyling + '';
-		}
-
-		return (
-			<button className={`${defaultStyling}`} onClick={item.action} key={index}>
-				{item.name}
-			</button>
-		);
-	};
+		[dispatch]
+	);
 
 	return (
 		<div className="bg-[#fff] p-4 rounded-lg shadow-md dark:bg-[#25273D]">
 			<div className="flex justify-between max-w-[50%] m-auto">
-				{filter.map((item, index) => renderButton(item, index))}
+				<button
+					className={
+						activeFilter === 'all' ? activeButtonStyle : buttonDefaultStyle
+					}
+					onClick={() => handleLoadAll('all')}
+				>
+					All
+				</button>
+				<button
+					className={
+						activeFilter === 'active' ? activeButtonStyle : buttonDefaultStyle
+					}
+					onClick={() => handleLoadActiveTodo('active')}
+				>
+					Active
+				</button>
+				<button
+					className={
+						activeFilter === 'completed' ? activeButtonStyle : buttonDefaultStyle
+					}
+					onClick={() => handleLoadCompletedTodo('completed')}
+				>
+					Completed
+				</button>
 			</div>
 		</div>
 	);
